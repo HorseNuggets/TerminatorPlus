08118bff577c8c934c6431b05a1c23715b8bd257
batchprogrammer314 <batchprogrammer314@gmail.com>
2021-06-26 20:22:22 -0500

    new descs 3

--- src/net/nuggetmc/ai/cmd/commands/ResetCommand.java
+++ src/net/nuggetmc/ai/cmd/commands/ResetCommand.java
@@ -19,7 +19,7 @@ public class ResetCommand implements CommandInterface {
     private final NPCManager manager;
 
     private final String name;
-    private final String description = "Information about loaded bots.";
+    private final String description = "Remove all loaded bots.";
     private final String cmdArgs = "";
 
     public ResetCommand() {
