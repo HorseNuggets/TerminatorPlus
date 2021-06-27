c1dc321f68b1320900086fd00d5f66ea83ccbf2f
batchprogrammer314 <batchprogrammer314@gmail.com>
2021-06-26 20:08:34 -0500

    added chatutils

---
+++ src/net/nuggetmc/ai/utils/ChatUtils.java
@@ -0,0 +1,7 @@
+package net.nuggetmc.ai.utils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class ChatUtils {
+    public static String LINE = ChatColor.GRAY + "------------------------------------------------";
+}
