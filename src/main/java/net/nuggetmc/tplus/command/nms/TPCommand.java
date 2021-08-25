cd869f21703327a25383d73ae1e16b8680b5a3a3
HyperSkys <66301237+HyperSkys@users.noreply.github.com>
2021-08-25 16:25:23 -0600

    Update TPCommand.java

--- src/main/java/net/nuggetmc/tplus/command/nms/TPCommand.java
+++ src/main/java/net/nuggetmc/tplus/command/nms/TPCommand.java
@@ -1,6 +1,4 @@
 package net.nuggetmc.tplus.command.nms;
 
-public class TPCommand {
-    // this class (as well as some others like "give", "effect", etc.) will listen into the PlayerCommandPreProcessEvent
-    // and will perform the actions accordingly to the bots as well as the real players
+public class TPCommand { // Unused class
 }
