c0218ee005b8d4907cc90469231a638062dc13a9
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2021-11-14 23:41:43 -0500

    Update UIManager.java

--- src/main/java/net/nuggetmc/tplus/ui/UIManager.java
+++ src/main/java/net/nuggetmc/tplus/ui/UIManager.java
@@ -1,12 +1,7 @@
 package net.nuggetmc.tplus.ui;
 
 import net.nuggetmc.tplus.bot.Bot;
-import net.nuggetmc.tplus.ui.menu.BotListGUI;
-import net.nuggetmc.tplus.ui.menu.BotManagerGUI;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
-import java.util.HashMap;
 
 public class UIManager {
     public static void openBotListGUI(Player p){
