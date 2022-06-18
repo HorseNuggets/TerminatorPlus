b89e1b13581250a53b071a876b6077558dd898ac
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-06-17 20:01:15 -0400

    make it compile

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,17 +2,34 @@ plugins {
     id("java")
 }
 
-group = "net.tplus"
+group = "net.nuggetmc"
 version = "3.1-BETA"
+
+val jarName = "TerminatorPlus-" + version;
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-
+    implementation(project(":TerminatorPlus-Plugin"))
+    implementation(project(":TerminatorPlus-API"))
 }
 
-tasks.getByName<Test>("test") {
-    useJUnitPlatform()
+tasks.jar {
+    from(configurations.compileClasspath.get().map { if (it.isDirectory()) it else zipTree(it) })
+    archiveFileName.set(jarName + ".jar")
+}
+//TODO currently, the resources are in src/main/resources, because gradle is stubborn and won't include the resources in TerminatroPlus-Plugin/src/main/resources, will need to fix
+
+/*
+task copyPlugin(type: Copy) {
+    from 'build/libs/' + jarName + '.jar'
+    into 'run/plugins'
+}
+ */
+
+tasks.register("copyPlugin", Copy::class.java) {
+    from("build/libs/" + jarName + ".jar")
+    into("run/plugins")
 }
