6610ac05db66d17cd0a836ddbd194dc800d57e74
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-06-17 20:02:28 -0400

    I didn't add the build scripts

---
+++ TerminatorPlus-Plugin/build.gradle.kts
@@ -0,0 +1,56 @@
+plugins {
+    `java-library`
+    id("io.papermc.paperweight.userdev") version "1.3.7"
+}
+
+group = "net.nuggetmc"
+version = "3.1-BETA"
+description = "TerminatorPlus"
+
+java {
+    // Configure the java toolchain. This allows gradle to auto-provision JDK 17 on systems that only have JDK 8 installed for example.
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+dependencies {
+    paperDevBundle("1.18.1-R0.1-SNAPSHOT")
+
+    //add the TerminatorPlus-API module
+    implementation(project(":TerminatorPlus-API"))
+
+    // paperweightDevBundle("com.example.paperfork", "1.19-R0.1-SNAPSHOT")
+
+    // You will need to manually specify the full dependency if using the groovy gradle dsl
+    // (paperDevBundle and paperweightDevBundle functions do not work in groovy)
+    // paperweightDevelopmentBundle("io.papermc.paper:dev-bundle:1.19-R0.1-SNAPSHOT")
+}
+
+tasks {
+    // Configure reobfJar to run when invoking the build task
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+
+        // Set the release flag. This configures what version bytecode the compiler will emit, as well as what JDK APIs are usable.
+        // See https://openjdk.java.net/jeps/247 for more information.
+        options.release.set(17)
+    }
+    javadoc {
+        options.encoding = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+    processResources {
+        filteringCharset = Charsets.UTF_8.name() // We want UTF-8 for everything
+    }
+
+    /*
+    reobfJar {
+      // This is an example of how you might change the output location for reobfJar. It's recommended not to do this
+      // for a variety of reasons, however it's asked frequently enough that an example of how to do it is included here.
+      outputJar.set(layout.buildDirectory.file("libs/PaperweightTestPlugin-${project.version}.jar"))
+    }
+     */
+}
+
